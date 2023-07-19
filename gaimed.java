03aeb43e7cc778953650b6fbf6cf5df2e9d499dd
simon438 <67150218+simon438@users.noreply.github.com>
2023-07-19 13:59:26 +0530

    Update gaimed.java

--- gaimed.java
+++ gaimed.java
@@ -1,3 +1,4 @@
 Things are beautiful and we can feel it seeing by idea
 Many of them are thinking that work is a workship
 But the thing is not they are following
+  Updating the file here today for practice
